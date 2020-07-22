7701e72d2a31d940bc84d650a3072a2707278142
leon <leon841208@gmail.com>
2020-07-22 17:09:43 +0800

    login api

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/all"; // 引入 all.scss 這裡 .scss 可省略不寫
+</style>
+
